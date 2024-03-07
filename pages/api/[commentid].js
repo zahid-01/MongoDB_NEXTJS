ce68ab74d87a97e72bd6892fc8d2543d494ad44b
zahid-01 <erzahidkhan101@gmail.com>
2024-03-07 10:40:44 +0530

    mongodb

--- pages/api/[commentid].js
+++ pages/api/[commentid].js
@@ -1,15 +1,28 @@
-const GetComments = (req, res) => {
+import { MongoClient } from "mongodb";
+
+const GetComments = async (req, res) => {
+  const client = new MongoClient(process.env.MONGODB_URL);
+  await client.connect();
+  const db = client.db();
+
   const { commentid } = req.query;
+
   if (req.method === "GET") {
+    const commentsCollection = db.collection("comments");
+
+    const comments = await commentsCollection.find();
     res.status(200).json({
       message: `Fetched comments for event with id ${commentid} `,
     });
   } else if (req.method === "POST") {
+    const comments = db.collection("comments");
+
     const comment = req.body;
+    const newComment = await comments.insertOne(comment);
 
     res.status(200).json({
       message: `Received the following comment for the event ${commentid}`,
-      comment,
+      newComment: "MISS",
     });
   }
 };
--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,16 +1,14 @@
 import { MongoClient } from "mongodb";
 const client = new MongoClient(process.env.MONGODB_URL);
-const dbName = "EVENTS";
 
 const handler = async (req, res) => {
   if (req.method === "POST") {
     const { email } = req.body;
 
     await client.connect();
-    const db = client.db(dbName);
+    const db = client.db();
 
     const subscribers = db.collection("mailList");
-    // const insertResult = await collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }]);
 
     const emailSubscriber = await subscribers.insertOne({ email });
 
